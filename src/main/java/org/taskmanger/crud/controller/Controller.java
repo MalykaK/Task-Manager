a5d7c4ba6e43289b68ae5374de29d3317278ee40
Sandeep <sdsts28@outlook.com>
2025-03-31 14:59:08 -0400

    added models package

--- src/main/java/org/taskmanger/crud/controller/Controller.java
+++ src/main/java/org/taskmanger/crud/controller/Controller.java
@@ -1,4 +1,4 @@
-package org.taskmanger.crud;
+package org.taskmanger.crud.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
