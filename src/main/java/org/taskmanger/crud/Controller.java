3c83bff5f63220898d1dc46496d18603fbd79ee9
Sandeep <sdsts28@outlook.com>
2025-03-31 14:54:38 -0400

    created first api. all get mappings

---
+++ src/main/java/org/taskmanger/crud/Controller.java
@@ -0,0 +1,24 @@
+package org.taskmanger.crud;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+
+    @GetMapping("/task/add/{addtask}")
+    public String boilWater(@PathVariable String addtask) {
+        return "Your Task for the " + addtask + " is initiated ";
+    }
+    @GetMapping("task/update/{updateTask}")
+    public String updateTask(@PathVariable String updateTask){
+        return "The Temperature of the " +updateTask+ " is 100 Degrees.";
+    }
+    @GetMapping("/task/delete/{deleteTask}")
+    public String deletetask(@PathVariable String deleteTask){
+        return "Your task is Deleted. The " +deleteTask+ " is done";
+    }
+}
